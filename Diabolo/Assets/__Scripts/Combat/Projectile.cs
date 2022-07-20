028d84e8130fdc51b9c3decea37f6ce344efb2ea
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-07-20 21:14:50 +0200

    Collision effect
    Collision effect to fire ball has been created and added.

--- Diabolo/Assets/__Scripts/Combat/Projectile.cs
+++ Diabolo/Assets/__Scripts/Combat/Projectile.cs
@@ -8,7 +8,11 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 20f;
+        [SerializeField] float destroyAfterTime = 2f;
         [SerializeField] bool isHoming = true;
+        [SerializeField] GameObject hitEffect = null;
+        [SerializeField] float maxLifeTime = 10f;
+        [SerializeField] GameObject[] destroyOnHit = null;
 
         Health target = null;
         float damage = 0f;
@@ -34,6 +38,8 @@ namespace RPG.Combat
         {
             this.target = target;
             this.damage = damage;
+
+            Destroy(gameObject, maxLifeTime);
         }
 
         private Vector3 GetAimLocation()
@@ -51,11 +57,22 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Health>() != target) { return; }
-
             if (target.IsDead()) { return; }
-
             target.TakeDamage(damage);
-            Destroy(gameObject, 2f);
+
+            speed = 0;
+
+            if (hitEffect != null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
+
+            foreach(GameObject toDestroy in destroyOnHit)
+            {
+                Destroy(toDestroy);
+            }
+
+            Destroy(gameObject, destroyAfterTime);
         }
     }
 }
